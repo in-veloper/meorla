aeb939b42b0abcfbd4c73c3690923b4d5596de91
in-veloper <yiniwinidev@gmail.com>
2023-11-03 18:01:21 +0900

    11.03 보건일정에 상단 알림영역 추가

--- src/views/WorkSchedule.js
+++ src/views/WorkSchedule.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { UncontrolledAlert } from "reactstrap";
 import MyCalendar from "../variables/calendar";
 
 function WorkSchedule() {
   return (
     <>
       <div className="content">
+        <UncontrolledAlert color="info" fade={false}>
+          <span>
+            <b>알림 &nbsp; </b>
+            This is a regular notification made with
+            color="info"
+          </span>
+        </UncontrolledAlert>
         <MyCalendar/>
       </div>
     </>
