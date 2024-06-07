5fcd09ea56d502079b909b2b673ba53583db8246
in-veloper <yiniwinidev@gmail.com>
2024-06-07 09:46:58 +0900

    6.7 pm2 환경 ecosystem 설정 변경 반영 commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,31 @@ module.exports = {
       args: '',           // 스크립트에 전달할 인자들
       watch: true,                 // 파일 변경 감지 후 자동 재시작
       env: {
-        COMMON_ENV_VARIABLE: 'true', // 모든 환경에서 적용될 환경 변수
+        NODE_ENV: 'development',
+        REACT_APP_BASE_PORT: 8000,
+        REACT_APP_BASE_ORIGIN: '*',
+        REACT_APP_BASE_URL: 'localhost:8000',
+        ACCESS_TOKEN_SECRET: 'jsfgfjguwrg8783wgbjs849h2fu3cnsvh8wyr8fhwfvi2g225',
+        REFRESH_TOKEN_SECRET: '825y8i3hnfjmsbv7gwajbl7fobqrjfvbs7gbfj2q3bgh8f42',
+        REACT_APP_MYSQL_HOST: 'localhost',
+        REACT_APP_MYSQL_USER: 'root',
+        REACT_APP_MYSQL_PASSWORD: 'yeeh01250412!@',
+        REACT_APP_MYSQL_DB: 'teaform_db'
+        // COMMON_ENV_VARIABLE: 'true', // 모든 환경에서 적용될 환경 변수
         // NODE_ENV: 'development',
         // PORT: 3000
       },
       env_production: {
-        NODE_ENV: 'production'     // 프로덕션 환경에서 적용될 환경 변수
+        NODE_ENV: 'production',     // 프로덕션 환경에서 적용될 환경 변수
+        REACT_APP_BASE_PORT: 8002,
+        REACT_APP_BASE_ORIGIN: 'http://223.130.130.53',
+        REACT_APP_BASE_URL: '223.130.130.53',
+        ACCESS_TOKEN_SECRET: 'jsfgfjguwrg8783wgbjs849h2fu3cnsvh8wyr8fhwfvi2g225',
+        REFRESH_TOKEN_SECRET: '825y8i3hnfjmsbv7gwajbl7fobqrjfvbs7gbfj2q3bgh8f42',
+        REACT_APP_MYSQL_HOST: '223.130.130.53',
+        REACT_APP_MYSQL_USER: 'root',
+        REACT_APP_MYSQL_PASSWORD: 'Yeeh01250412!@',
+        REACT_APP_MYSQL_DB: 'teaform_db'
         // PORT: 8000
       }
     }]
