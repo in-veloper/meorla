d59b188c004c56ac284909343ddfa50879c0c9da
in-veloper <yiniwinidev@gmail.com>
2024-06-11 11:36:37 +0900

    6.11 새로고침 시 로그인 페이지 이동 문제 수정 commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { UserProvider } from "contexts/UserContext";
-// import { MedicineProvider } from "contexts/MedicineContext";
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/paper-dashboard.scss?v=1.3.0";
 import "assets/demo/demo.css";
@@ -10,34 +9,26 @@ import "perfect-scrollbar/css/perfect-scrollbar.css";
 import AdminLayout from "layouts/Admin.js";
 import Login from "views/Login";
 import { connectSocket } from "components/Socket/socket";
-import PrivateRoute from "components/Route/PrivateRoute";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const serverUrl = `${BASE_URL}`;
 
-const AppRoutes = () => {
+const App = () => {
   useEffect(() => {
     connectSocket(serverUrl);
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Navigate to="/meorla/dashboard" replace />} />
-      <Route path="/meorla/*" element={<PrivateRoute><AdminLayout /></PrivateRoute>} />
-    </Routes>
-  );
-};
-
-const App = () => {
-  return (
     <BrowserRouter>
       <UserProvider>
-        <AppRoutes />
+          <Routes>
+            <Route path="/" element={<Login/>} />
+            <Route path="/meorla/*" element={<AdminLayout />} />
+            <Route path="/dashboard" element={<Navigate to="/meorla/dashboard" replace />} />
+          </Routes>
       </UserProvider>
     </BrowserRouter>
   );
 };
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
